66882d8383d9eaa24c104ac15776a3646445f1e9
William Gabriel Braga <williamgabrielbraga14@gmail.com>
2022-10-17 21:05:47 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
-var lastPage = ;
+var lastBtn = 'home';
 
 
 function changePage(id){
     let btn = document.getElementById(id);
-    let page = document.getElementById(`p${id}`)
+    let page = document.getElementById(`p${id}`);
+    let lastButton = document.getElementById(lastBtn);
+    let lastPage = document.getElementById(`p${lastBtn}`);
     for (let c = 0; c < btn.classList.length; c++){
         if (btn.classList[c] == 'pSelectionBtn-clicked'){
             return false;
         }
     }
     btn.classList.add('pSelectionBtn-clicked');
-    page.classList
-    
+    lastButton.classList.remove('pSelectionBtn-clicked');
+    page.classList.remove('hide');
+    lastPage.classList.add('hide');
+    lastBtn = id;
 }
